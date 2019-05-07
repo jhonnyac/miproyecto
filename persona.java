ebccf8295b2ba99fac4ebd0279e22917598602b8
Shur <infoemergentes@gmail.com>
2019-05-07 19:30:01 -0400

    Primer commit

---
+++ persona.java
@@ -0,0 +1,20 @@
+public class Persona
+{ private int id;
+private String nombre;
+private String telefono;
+
+public persona(){
+
+
+}
+public int getid()
+{
+	return this.id;
+
+}
+public void setid(int id)
+{
+	this.id = id;
+}
+
+}
---
+++ producto.java
@@ -0,0 +1,6 @@
+public class producto
+{
+	private int id;
+	private String descripcion;
+	private int cantidad;
+}
